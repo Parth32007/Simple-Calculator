5596646637d0fd71dcffd7d95bf23551d442772b
Parth Tambe <parthtambe327@gmail.com>
2026-03-20 14:07:35 +0530

    final calculator deployment

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    data = request.json
+    expression = data.get("expression","")
+
+    try:
+        result = str(eval(expression))
+    except:
+        result = "Error"
+
+    return jsonify({"result": result})
+
+if __name__ == "__main__":
+    app.run()
